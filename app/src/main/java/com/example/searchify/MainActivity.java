de4766459e4cf9cff313947f3f06730f060b918a
amit-099 <amitcsedu99@gmail.com>
2019-04-05 02:19:36 +0600

    Initial commit

---
+++ app/src/main/java/com/example/searchify/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.searchify;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(MainActivity.this, OwnerSignUpActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
